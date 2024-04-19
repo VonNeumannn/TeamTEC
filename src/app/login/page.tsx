ce0e5de25f24761e9bbbc6db1fb947a9f034d526
VonNeumannn <justingilberto2000@gmail.com>
2024-04-19 13:29:54 -0600

    feat: adding folder structure and example

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import styles from "../page.module.css";
+
+export default function LoginPage() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>
+                <p>
+                    Este debería ser el Login
+                </p>
+            </div>
+        </main>
+    );
+}
