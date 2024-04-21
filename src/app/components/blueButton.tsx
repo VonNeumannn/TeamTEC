05667bfc151680d6807e67987020294bfb0c13d4
joctan-tec <joctan@estudiantec.cr>
2024-04-21 03:41:30 -0600

    feat: login screen finished

---
+++ src/app/components/blueButton.tsx
@@ -0,0 +1,18 @@
+'use client'
+import styles from '../page.module.css';
+interface BlueButtonProps {
+    text: string;
+    onClick: () => void;
+  }
+
+
+export function BlueButton(BlueButtonProps: BlueButtonProps) {
+    const { text, onClick } = BlueButtonProps;
+    
+    return (
+      <button className={styles.blueButton} onClick={onClick}>
+        {text}
+      </button>
+    );
+  }
+  
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,28 @@
+'use client'
 import styles from "../page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import UserLogo from "../../../public/circle-user-round.svg";
+import { BlueButton } from "../components/blueButton";
 
 export default function LoginPage() {
     return (
         <main className={styles.main}>
-            <div className={styles.description}>
-                <p>
-                    Este debería ser el Login
-                </p>
+            <div className={styles.loginContainer}>
+                <Image src={UserLogo} alt="User Logo" />
+                <h1>Login</h1>
+                <form className={styles.formContainer}>
+                    <div className={styles.formGroup}>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" required placeholder="..."/>
+                    </div>
+                    <div className={styles.formGroup}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" required placeholder="..." />
+                    </div>
+                    <Link href="#" >Olvidó su contraseña</Link>
+                    <BlueButton text="LOGIN" onClick={() => {alert("Le clickeaste al boton")}} />
+                </form>
             </div>
         </main>
     );
