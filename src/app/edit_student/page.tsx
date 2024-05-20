785a641d9e932a68d1755efefccfef7d4b15f390
VonnNeumannn <justingilberto@gmail.com>
2024-05-20 14:55:22 -0600

    fix: fixing small bugs

--- src/app/edit_student/page.tsx
+++ src/app/edit_student/page.tsx
@@ -8,22 +8,24 @@ import Estudiante from "../../model/Estudiante";
 
 export default function StudentEdit() {
     const router = useRouter();
-    const [loadData, setloadData]= useState<Estudiante[]>([]);
+    const [loadData, setloadData] = useState<Estudiante[]>([]);
     const [data, setData] = useState({
         carne: '',
-        nombre: '', 
+        nombre: '',
         primerApellido: '',
         segundoApellido: '',
         correo: '',
         celular: ''
     });
     let carne = '';
-    const storedData = localStorage.getItem("student");
-    if (storedData) {
-        const studentData = JSON.parse(storedData);
-        carne = studentData.carne;
-    } else {
-        console.log("No data found in localStorage for key 'student'");
+    if (typeof window !== 'undefined') {
+        const storedData = localStorage.getItem("student");
+        if (storedData) {
+            const studentData = JSON.parse(storedData);
+            carne = studentData.carne;
+        } else {
+            console.log("No data found in localStorage for key 'student'");
+        }
     }
 
     useEffect(() => {
@@ -41,8 +43,8 @@ export default function StudentEdit() {
     useEffect(() => {
         if (loadData.length > 0) {
             setData({
-                carne: loadData[0].carne, 
-                nombre: loadData[0].nombre, 
+                carne: loadData[0].carne,
+                nombre: loadData[0].nombre,
                 primerApellido: loadData[0].primerApellido,
                 segundoApellido: loadData[0].segundoApellido,
                 correo: loadData[0].correo,
@@ -51,8 +53,8 @@ export default function StudentEdit() {
         }
     }, [loadData]);
 
-    
-    
+
+
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setData({
@@ -62,7 +64,7 @@ export default function StudentEdit() {
     };
 
     function handleEdit() {
-        const student: Estudiante = new Estudiante (
+        const student: Estudiante = new Estudiante(
             data.carne,
             data.nombre,
             data.primerApellido,
@@ -70,54 +72,54 @@ export default function StudentEdit() {
             data.correo,
             data.celular,
             loadData[0].sede
-          );
+        );
         handlerUpdateController(loadData[0].carne, student);
-        router.push('/viewStudents'); 
+        router.push('/viewStudents');
         // Aquí puedes agregar el código para editar el item
     }
 
-    
 
-  return (
-    <main className={styles.main} id="main">
-        <div className={styles.studentEditContainer}>
-            <h1>Editar Estudiante</h1>
+
+    return (
+        <main className={styles.main} id="main">
+            <div className={styles.studentEditContainer}>
+                <h1>Editar Estudiante</h1>
                 <div className={styles.formstudentEdit}>
                     <form className={styles.formContainerstudentEdit}>
 
                         <div className={styles.formGroupStudentEdit}>
                             <label htmlFor="carne">Carne</label>
-                            <input type="text" id="carne" name="carne" required placeholder="..." value={data.carne} onChange={handleChange}/>
+                            <input type="text" id="carne" name="carne" required placeholder="..." value={data.carne} onChange={handleChange} />
                         </div>
 
                         <div className={styles.formGroupStudentEdit}>
                             <label htmlFor="name">Nombre</label>
-                            <input type="text" id="nombre" name="name" placeholder="..." value={data.nombre} onChange={handleChange}/>
+                            <input type="text" id="nombre" name="name" placeholder="..." value={data.nombre} onChange={handleChange} />
                         </div>
                         <div className={styles.formGroupStudentEdit}>
                             <label htmlFor="lastName1">Primer Apelldio</label>
-                            <input type="text" id="primerApellido" name="lastName1" required placeholder="..." value={data.primerApellido} onChange={handleChange}/>
+                            <input type="text" id="primerApellido" name="lastName1" required placeholder="..." value={data.primerApellido} onChange={handleChange} />
                         </div>
                         <div className={styles.formGroupStudentEdit}>
                             <label htmlFor="lastName2">Segundo Apellido</label>
-                            <input type="text" id="segundoApellido" name="lastName2" required placeholder="..." value={data.segundoApellido} onChange={handleChange}/>
+                            <input type="text" id="segundoApellido" name="lastName2" required placeholder="..." value={data.segundoApellido} onChange={handleChange} />
                         </div>
                         <div className={styles.formGroupStudentEdit}>
                             <label htmlFor="email">Correo</label>
-                            <input type="text" id="correo" name="email" placeholder="..." value={data.correo} onChange={handleChange}/>
+                            <input type="text" id="correo" name="email" placeholder="..." value={data.correo} onChange={handleChange} />
                         </div>
                         <div className={styles.formGroupStudentEdit}>
                             <label htmlFor="cellphone">Número celular</label>
-                            <input type="tel" id="celular" name="cellphone" required placeholder="..." value={data.celular} onChange={handleChange}/>
+                            <input type="tel" id="celular" name="cellphone" required placeholder="..." value={data.celular} onChange={handleChange} />
                         </div>
                     </form>
                 </div>
-                
-            <div style={{ display: 'flex', gap: '10px' }}> {/* Contenedor de botones */}
-                <button className={styles.greenButton} onClick={() =>  handleEdit()} style={{ width: '120px' }} >Guardar</button>
-                <button className={styles.deleteButton} onClick={() => {router.push('/viewStudents'); }} style={{ width: '120px' }} >Cancelar</button>
+
+                <div style={{ display: 'flex', gap: '10px' }}> {/* Contenedor de botones */}
+                    <button className={styles.greenButton} onClick={() => handleEdit()} style={{ width: '120px' }} >Guardar</button>
+                    <button className={styles.deleteButton} onClick={() => { router.push('/viewStudents'); }} style={{ width: '120px' }} >Cancelar</button>
+                </div>
             </div>
-        </div>
-    </main>
-  );
+        </main>
+    );
 }
