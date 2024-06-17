5f341c8a64fb95690b3cca02342766f444256005
Manuel Rodríguez <m.alejandro00@estudiantec.cr>
2024-06-17 16:10:25 -0600

    Calendario (tiene un problema)

--- src/app/mainMenu/page.tsx
+++ src/app/mainMenu/page.tsx
@@ -273,6 +273,8 @@ export default function MainMenuPage() {
                             <BlueButton text="Itinerario" onClick={() => { router.push('/itineraries') }} type="button" />
                             <BlueButton text="Registrar profesor" onClick={() => { router.push('/professor_register') }} type="button" />
                             <BlueButton text="Perfil estudiante" onClick={() => { router.push('/students_profile') }} type="button" />
+                            <BlueButton text="Calendario" onClick={() => { router.push('/calendario') }} type="button" />
+
                         </div>
 
                     </div>
