61956592ba0dbd467b90feb3e797eeb944512f8b
mauricio <mauriciocamposc1@gmail.com>
2024-06-17 00:28:54 -0600

    fix: fecha

--- src/app/visitor/ActivityVisitor.ts
+++ src/app/visitor/ActivityVisitor.ts
@@ -7,7 +7,7 @@ export class ActivityVisitor implements Visitor{
     visit(itinerary: itinerarioData, activity: activityData, localTime: Date): void {
 
         //si es planeada y la fecha es menor o igual a la fecha del sistema, cambiar a notificada
-        if (activity.estado === "Planeada" && new Date(activity.fecha) <= localTime) {
+        if (activity.estado === "Planeada" && new Date(activity.iniciarRecordatorio) <= localTime) {
             //cambiar estado a notificada en bd
             //aqui paso el nombre del itinerario, pero deberia ser el id
             searchIdItineraryByName(itinerary.nombre).then((id) => { //id se refiere al id del itinerario
--- src/app/visitor/ReminderVisitor.ts
+++ src/app/visitor/ReminderVisitor.ts
@@ -7,10 +7,28 @@ import { itinerarioData } from "@/controller/ItinerarioController";
 export class ReminderVisitor implements Visitor{
     visit(itinerary: itinerarioData, activity: activityData, localTime: Date): void {
         //si es planeada y la fecha es menor o igual a la fecha del sistema, cambiar a notificada
+        let fechaActividad = new Date(activity.iniciarRecordatorio);
+        let frecuencia = activity.frecuencia;
+        let fechaActual = new Date(localTime);
 
-        if (activity.estado === "Notificada" && new Date(activity.fecha) <= localTime) {
-            //supongo que aqui mandamos una alerta
-            console.log("Recordatorio de actividad");
+        if (activity.estado === "Notificada" && fechaActividad <= fechaActual) { 
+            //recordatorio segun la frecuencia, tomando la fecha de la actividad y la actual * frecuencia
+
+            // Convertir ambas fechas a objetos Date
+            const d1 = new Date(fechaActividad);
+            const d2 = new Date(fechaActual);
+            
+            // Calcular la diferencia en milisegundos
+            const differenceInTime = d2.getTime() - d1.getTime();
+            
+            // Convertir la diferencia de milisegundos a días
+            const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+
+            // Si la diferencia en días es igual a la frecuencia, enviar recordatorio
+            if (differenceInDays % frecuencia == 0) {
+                //metan aqui el codigo para enviar el recordatorio
+                console.log("Recordatorio de actividad");
+            }
         }
     }
 }
